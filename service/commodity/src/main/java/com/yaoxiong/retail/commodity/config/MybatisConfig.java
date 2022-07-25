239231eb903b937896ea31cad65bc2fafaf37e48
yaoxiong <trmylf2@yahoo.com>
2022-07-25 00:30:51 -0400

    seata xa mode

--- service/commodity/src/main/java/com/yaoxiong/retail/commodity/config/MybatisConfig.java
+++ service/commodity/src/main/java/com/yaoxiong/retail/commodity/config/MybatisConfig.java
@@ -2,7 +2,7 @@ package com.yaoxiong.retail.commodity.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
-import io.seata.rm.datasource.DataSourceProxy;
+import io.seata.rm.datasource.xa.DataSourceProxyXA;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -37,7 +37,7 @@ public class MybatisConfig {
      */
     @Primary
     @Bean("dataSource")
-    public DataSourceProxy dataSourceProxy(DataSource druidDataSource) {
-        return new DataSourceProxy(druidDataSource);
+    public DataSource  dataSource(DataSource druidDataSource) {
+        return new DataSourceProxyXA(druidDataSource);
     }
 }
--- service/order/src/main/java/com/yaoxiong/retail/order/config/MybatisConfig.java
+++ service/order/src/main/java/com/yaoxiong/retail/order/config/MybatisConfig.java
@@ -2,7 +2,7 @@ package com.yaoxiong.retail.order.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
-import io.seata.rm.datasource.DataSourceProxy;
+import io.seata.rm.datasource.xa.DataSourceProxyXA;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -38,7 +38,7 @@ public class MybatisConfig {
      */
     @Primary
     @Bean("dataSource")
-    public DataSourceProxy dataSourceProxy(DataSource druidDataSource) {
-        return new DataSourceProxy(druidDataSource);
+    public DataSource  dataSource(DataSource druidDataSource) {
+        return new DataSourceProxyXA(druidDataSource);
     }
 }
